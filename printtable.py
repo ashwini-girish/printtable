94615b92760b626199af45501d2bb46dcfa9dee2
Ashwini <70182126+ashwini-girish@users.noreply.github.com>
2020-08-25 11:00:47 +0200

    Update printtable.py

--- printtable.py
+++ printtable.py
@@ -9,15 +9,5 @@ def multtable(start, stop, number):
     print ('')
         #output: 1 x 7 = 7
 
-def powertable(power, i):
-    """
-    Prints power table for <i> to the power of <power>
-    """
-    print ('Power table')
-    for x in range (1, i + 1):
-        print (f"{i} ^ {power} = {i**power}")
-    print ('')
-
 if __name__ == '__main__':
     multtable(1, 4, 7)
-    powertable(2, 4)
